688a5b87d052e5be13131aa52a85b3ed12924e44
Tom Yeh <doubleshow@gmail.com>
2015-01-24 10:08:10 -0700

    more stuff to cv

--- wintersmith/process.js
+++ wintersmith/process.js
@@ -5,6 +5,14 @@ var Parser = require('parse-xlsx'),
 
 var src = './data/cv.xlsx'
 
+
+function toJson(){
+	return new through2.obj(function(chunk, enc, callback) {
+        var record = JSON.parse(chunk)        
+        callback(null, record)
+    })
+}
+
 new Parser(src, 'publications')
     .recordStream
     .pipe(new through2.obj(function(chunk, enc, callback) {
@@ -20,17 +28,44 @@ new Parser(src, 'publications')
         fs.writeFileSync('contents/publications.json', JSON.stringify(data, null, ' '))
     }))
 
+new Parser(src, 'education')
+    .recordStream
+    .pipe(toJson())
+    .pipe(concat(function(data) {
+        fs.writeFileSync('contents/education.json', JSON.stringify(data, null, ' '))
+    }))    
+
+new Parser(src, 'phd')
+    .recordStream
+    .pipe(toJson())
+    .pipe(concat(function(data) {
+        fs.writeFileSync('contents/phd.json', JSON.stringify(data, null, ' '))
+    }))      
+
+new Parser(src, 'awards')
+    .recordStream
+    .pipe(toJson())
+    .pipe(concat(function(data) {
+        fs.writeFileSync('contents/awards.json', JSON.stringify(data, null, ' '))
+    }))  
+
 new Parser(src, 'grants')
     .recordStream
-    .pipe(new through2.obj(function(chunk, enc, callback) {
-        var record = JSON.parse(chunk)
-        if ('authors' in record) {
-            record['authors'] = record['authors'].replace(',', ';').split(';')
-
-        }
-        console.log(record)
-        callback(null, record)
-    }))
+    .pipe(toJson())
     .pipe(concat(function(data) {
         fs.writeFileSync('contents/grants.json', JSON.stringify(data, null, ' '))
     }))    
+
+new Parser(src, 'service')
+    .recordStream
+    .pipe(toJson())
+    .pipe(concat(function(data) {
+        fs.writeFileSync('contents/service.json', JSON.stringify(data, null, ' '))
+    }))        
+
+new Parser(src, 'teaching')
+    .recordStream
+    .pipe(toJson())
+    .pipe(concat(function(data) {
+        fs.writeFileSync('contents/teaching.json', JSON.stringify(data, null, ' '))
+    }))            
